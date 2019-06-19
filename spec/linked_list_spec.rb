ad3efc04b52d8e4922ea3c45883db68502ab9423
Vince Carollo <vincecarollo@gmail.com>
2019-06-19 09:55:52 -0600

    Add spec for linked list prpend and insert

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -43,4 +43,32 @@ RSpec.describe 'A Linked List' do
     expect(@linked_list.to_string).to eq("doop poop")
   end
 
+  it 'can prepend nodes' do
+    @linked_list.append("plop")
+    @linked_list.append("suu")
+    @linked_list.prepend("dop")
+    expect(@linked_list.to_string).to eq('dop plop suu')
+    expect(@linked_list.count).to eq(3)
+  end
+
+  it 'can insert nodes into given position' do
+    @linked_list.append("plop")
+    @linked_list.append("suu")
+    @linked_list.prepend("dop")
+    @linked_list.insert(1, 'woo')
+
+    expected = "dop woo plop suu"
+    expect(@linked_list.to_string).to eq(expected)
+  end
+
+  it 'can insert nodes into different position' do
+    @linked_list.append("plop")
+    @linked_list.append("suu")
+    @linked_list.prepend("dop")
+    @linked_list.insert(2, 'woo')
+
+    expected = "dop plop woo suu"
+    expect(@linked_list.to_string).to eq(expected)
+  end
+
 end
