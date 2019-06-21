ed19ddc232c949c671ba755f89aa9cff2e8be123
Vince Carollo <vincecarollo@gmail.com>
2019-06-21 09:25:10 -0600

    Add functionality to validate each beat before adding and speed and voice changes for users

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,15 +1,45 @@
+require './lib/linked_list'
+
 class JungleBeat
   attr_reader :list
+  attr_accessor :rate, :voice
 
-  def initialize
+  def initialize(data=nil)
     @list = LinkedList.new
+    @validation = 'tee dee deep bop boop la na doo ditt woo hoo shu dop'
+    @rate = 500
+    @voice = 'Alex'
+    self.append(data) if !data.nil?
+  end
+
+  def reset_rate
+    @rate = 500
+  end
+
+  def reset_voice
+    @voice = "Alex"
   end
 
   def append(data)
+    add_count = 0
     data.split.each do |data|
-      @list.append(data)
+      if @validation.split.include?(data)
+        @list.append(data)
+        add_count += 1
+      end
     end
-    data
+    add_count
+  end
+
+  def prepend(data)
+    add_count = 0
+    data.split.each do |data|
+      if @validation.split.include?(data)
+        @list.prepend(data)
+        add_count += 1
+      end
+    end
+    add_count
   end
 
   def count
@@ -17,6 +47,11 @@ class JungleBeat
   end
 
   def play
-    `say -r 180 #{@list.to_string}`
+    `say -r #{@rate} -v #{@voice} #{@list.to_string}`
+    @list.count
+  end
+
+  def all
+    @list.to_string
   end
 end
