bd4187b51dde497cc9633873b8a99a9f6aa1a861
Vince Carollo <vincecarollo@gmail.com>
2019-06-21 09:24:38 -0600

    Add spec for beat validation and spped and voice user changes

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -16,11 +16,11 @@ RSpec.describe 'A Jungle Beat' do
   end
 
   it 'can append beats as linked lists' do
-    expect(@jb.append('deep doo ditt')).to eq('deep doo ditt')
+    expect(@jb.append('deep doo ditt')).to eq(3)
     expect(@jb.list.head.data).to eq('deep')
     expect(@jb.list.head.next_node.data).to eq('doo')
 
-    expect(@jb.append('woo hoo shu')).to eq('woo hoo shu')
+    expect(@jb.append('woo hoo shu')).to eq(3)
     expect(@jb.count).to eq(6)
   end
 
@@ -28,6 +28,26 @@ RSpec.describe 'A Jungle Beat' do
     @jb.append('deep doo ditt woo hoo shu')
     expect(@jb.count).to eq(6)
     expect(@jb.list.count).to eq(6)
+  end
+
+  it 'cant add invalid beats' do
+    @jb = JungleBeat.new('deep')
+
+    expect(@jb.append('Mississippi')).to eq(0)
+    expect(@jb.all).to eq('deep')
+    expect(@jb.prepend('tee tee tee Mississippi')).to eq(3)
+    expect(@jb.all).to eq('tee tee tee deep')
+  end
+
+  it 'can set speed of beats' do
+    @jb = JungleBeat.new('deep dop dop deep')
+    expect(@jb.play).to eq(4)
+    @jb.rate = 100
+    @jb.voice = 'Alice'
+    expect(@jb.play).to eq(4)
+    expect(@jb.reset_rate).to eq(500)
+    expect(@jb.reset_voice).to eq('Alex')
+    expect(@jb.play).to eq(4)
   end
 
 end
